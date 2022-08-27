b4b2f5110057a9e4416afd8dd942511014643011
Waqar Saleem <wsaleem@gmail.com>
2022-08-27 11:28:12 +0500

    first commit

---
+++ dfa.py
@@ -0,0 +1,25 @@
+class DFA:
+    def __init__(self, dfa: [str]) -> None:
+        '''Creates this DFA as described in dfa.
+
+        Parameters:
+        - self: obligatory reference to this object
+        - dfa: description of the desired DFA
+
+        Return:
+        None
+        '''
+        pass
+
+    def accepts(self, w: str) -> bool:
+        '''Returns the acceptance status of w.
+
+        Parameters:
+        - self: obligatory reference to this object
+        - w: the string to check
+
+        Return:
+        True if this DFA accepts w, False otherwise.
+        '''
+        pass
+
